bb88e06bc8cb0dd22ae90f3cc0602cf59276d737
Mridul2820 <immridulpanda2820@gmail.com>
2022-02-13 15:07:14 +0530

    Links added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,94 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import { AiOutlineGithub, AiOutlineInstagram, AiOutlineTwitter, AiFillLinkedin } from 'react-icons/ai';
+import { BiWorld } from 'react-icons/bi';
+import { MdArrowForwardIos } from 'react-icons/md';
+import { v4 as uuidv4 } from 'uuid';
 
-const Home = () => {
+const linkItems = [
+    {
+        heading: "Mridul.tech",
+        subtext: "Visit Website",
+        icon: <BiWorld size={36} />,
+        color: "#fff",
+        bg: "#0f81f0",
+        url: 'https://www.mridul.tech/'
+    },
+    {
+        heading: "Mridul2820",
+        subtext: "Follow on Github",
+        icon: <AiOutlineGithub size={36} />,
+        color: "#fff",
+        bg: "#333",
+        url: 'https://github.com/Mridul2820'
+    },
+    {
+        heading: "@i_mridul",
+        subtext: "Follow on Instagram",
+        icon: <AiOutlineInstagram size={36} />,
+        color: "#fff",
+        bg: "#E4405F",
+        url: 'https://instagram.com/i_mridul'
+    },
+    {
+        heading: "@i_mridul",
+        subtext: "Follow on Twitter",
+        icon: <AiOutlineTwitter size={36} />,
+        color: "#fff",
+        bg: "#0f81f0",
+        url: 'https://twitter.com/i_mridul'
+    },
+    {
+        heading: "Mridul Panda",
+        subtext: "Follow on Linkedin",
+        icon: <AiFillLinkedin size={36} />,
+        color: "#fff",
+        bg: "#0f81f0",
+        url: 'https://twitter.com/i_mridul'
+    }
+]
+
+const AllLinks = () => {
     return (
-        <div className='font-bold'>
-            App
+        <div className='p-5 flex flex-col gap-5 justify-center items-center'>
+            <div className='rounded-full w-20 h-20 overflow-hidden shadow-bs1'>
+                <Image 
+                    src="/mridul.jpg" 
+                    alt="logo"
+                    height={80} 
+                    width={80} 
+                    objectFit='cover'
+                    priority={true}
+                />
+            </div>
+            {linkItems.map(item => (
+                <a
+                    href={item.url}
+                    key={uuidv4()} 
+                    className="flex items-center justify-between max-w-[400px] w-full rounded-md shadow-bs3 hover:shadow-bs1 transition-all duration-300 px-4 py-2 select-none hover:opacity-90"
+                    target='_blank'
+                    rel='noreferrer'
+                    style={{
+                        background: item.bg,
+                        color: item.color
+                    }}
+                >
+                    <div className="flex items-center gap-3">
+                        {item.icon}
+                        <div className="">
+                            <span className='block font-semibold text-xl'>
+                                {item.heading}
+                            </span>
+                            <span className='block font-light'>
+                                {item.subtext}
+                            </span>
+                        </div>
+                    </div>
+                    <MdArrowForwardIos size={30} />
+                </a>
+            ))}
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default AllLinks;
